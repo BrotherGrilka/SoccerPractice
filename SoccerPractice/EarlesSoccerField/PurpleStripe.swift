c1fc25f4f2edd29a49efd77eddf3937f90348a22
BrotherGrilka <exweerdo@gmail.com>
2014-12-05 16:24:58 -0500

    Go team!

---
+++ SoccerPractice/EarlesSoccerField/PurpleStripe.swift
@@ -0,0 +1,35 @@
+//
+//  PurpleStripe.swift
+//  EarlesSoccerField
+//
+//  Created by Dunc on 12/4/14.
+//  Copyright (c) 2014 Mmyrmidons. All rights reserved.
+//
+
+import UIKit
+
+class PurpleStripe: Player {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            playerImage.image = UIImage(named: "PurpleStripe.png");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
